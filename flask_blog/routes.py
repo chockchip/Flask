0101f48f1000058a6e79278e1bba319592a6261c
watcharapon-w <watcharapon.won@stu.nida.ac.th>
2021-10-16 00:12:24 +0700

    learning day 2

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,7 +1,10 @@
-from flask import render_template, url_for, flash, redirect
-from flask_blog import app
-from flask_blog.form import LoginForm, RegistrationForm
+from flask import render_template, url_for, flash, redirect, request
+from wtforms.fields.core import LocaleAwareNumberField
+from wtforms.validators import Email
+from flask_blog import app, db, bcrypt
+from flask_blog.form import LoginForm, RegistrationForm, UpdateAccountForm
 from flask_blog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
 
@@ -29,19 +32,44 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account create for {form.username.data}!", "success")
-        return redirect(url_for("home"))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your account has been created! You are now able to login", "success")
+        return redirect(url_for("login"))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "admin@blog.com" and form.password.data == "password":
-            flash(f"You have been logedin!", "success")
-            return redirect(url_for("home"))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
-            flash(f"Login Unsucessful. Please check username and password", "danger")
+            flash(f"Login Unsucessful. Please check email and password", "danger")
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route("/account", methos=["GET", "POST"])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.is_validate_on_submit():
+        current_user.username = form.username.data
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template('account.html', title='account', image_file=image_file, form=form)
--- run.py
+++ run.py
@@ -5,6 +5,5 @@ Everything you would like to import from package. It have to declare in the
 package already
 '''
 
-
 if __name__ == '__main__':
     app.run(debug=True)
